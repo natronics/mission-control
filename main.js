a43d1721fee806f24ef6a073e7e2ce780d4459e0
Nathan Bergey <nathan.bergey@gmail.com>
2016-07-12 18:46:10 -0600

    HTML: Add some more page elements

--- main.js
+++ main.js
@@ -2,7 +2,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world!
+      </div>
+    );
+  }
+});
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
+  <CommentBox />,
+  document.getElementById('content')
 );
