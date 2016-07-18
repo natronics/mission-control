4b85cf9338bde0cfcd6b90d36360ea98d7250788
Nathan Bergey <nathan.bergey@gmail.com>
2016-07-18 11:11:52 -0600

    Clean up page a little, make it very sparse.

--- src/main.js
+++ src/main.js
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Style from './style/style.sass';
 import SelectStyle from 'react-select/dist/react-select.min.css';
-import ConnectionSelect from './connections.js';
 import Data from './telemetry.js';
-
-ReactDOM.render(
-  <ConnectionSelect/>,
-  document.getElementById('connections')
-);
 
 ReactDOM.render(
   <Data/>,
