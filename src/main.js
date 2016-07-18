0aa9e70b9f8f95aa23bc91779f01e137c1a80f5e
Nathan Bergey <nathan.bergey@gmail.com>
2016-07-18 11:20:34 -0600

    Blank Page: restart building telemetry control from a blank page

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Style from './style/style.sass';
-import SelectStyle from 'react-select/dist/react-select.min.css';
-import Data from './telemetry.js';
+
+import Page from './telemetry-page.js'
 
 ReactDOM.render(
-  <Data/>,
-  document.getElementById('data')
+  <Page/>,
+  document.getElementById('page')
 );
---
+++ src/telemetry-page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Page extends React.Component {
+  render() {
+    return (
+        <section className="connection">
+          <div className="container">
+            <p>page</p>
+          </div>
+        </section>
+    );
+  }
+};
+
+module.exports = Page;
